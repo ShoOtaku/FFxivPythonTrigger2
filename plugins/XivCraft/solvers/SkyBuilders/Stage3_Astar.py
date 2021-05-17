7cc8f5a0a03ff27ff5b0581a71236256f3598de1
OTAKUSHO <xiaoshaoxuan@gmail.com>
2021-05-17 09:40:53 +0800

    solver update and network plugin

--- plugins/XivCraft/solvers/SkyBuilders/Stage3_Astar.py
+++ plugins/XivCraft/solvers/SkyBuilders/Stage3_Astar.py
@@ -40,7 +40,7 @@ def allowSkills(craft):
             to_add.append('坯料加工')
             if '俭约' not in craft.effects:
                 to_add.append('俭约加工')
-        if craft.current_cp < 80:
+        if craft.current_cp < 80 and '内静' in craft.effects:
             to_add.append('比尔格的祝福')
         for skill in to_add:
             if craft.current_durability > craft.get_skill_durability(skill) and craft.current_cp >= craft.get_skill_cost(skill):
@@ -103,12 +103,15 @@ class Stage3:
     def __init__(self):
         self.queue = []
         self.is_first = True
+        self.prev_skill = None
 
     def is_finished(self, craft, prev_skill=None):
+        if craft.current_quality >= craft.recipe.max_quality:
+            return True
         if self.is_first:
             debug("solver astar", "try to solve:\n%s" % craft.simple_str())
             self.is_first = False
-        if not bool(self.queue) or craft.status.name in SpecialStatus:
+        if not bool(self.queue) or craft.status.name in SpecialStatus or prev_skill != self.prev_skill:
             start = time.perf_counter()
             ans = try_solve(craft, TIME_LIMIT)
             if ans[1]:
@@ -117,7 +120,8 @@ class Stage3:
         return not bool(self.queue)
 
     def deal(self, craft, prev_skill=None):
-        return self.queue.pop(0)
+        self.prev_skill = self.queue.pop(0)
+        return self.prev_skill
 
 
 class StageEnd:
